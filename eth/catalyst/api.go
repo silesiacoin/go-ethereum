b629e2560aae26059bf69ef19690385e078cbefd
Marius van der Wijden <m.vanderwijden@live.de>
2021-11-11 15:37:20 +0100

    eth/catalyst: fix trigger transition

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -504,6 +504,11 @@ func (api *ConsensusAPI) setHead(newHead common.Hash) error {
 	}
 	headBlock := api.eth.BlockChain().CurrentBlock()
 	if headBlock.Hash() == newHead {
+		// Trigger the transition if it's the first `NewHead` event.
+		merger := api.merger()
+		if !merger.PoSFinalized() {
+			merger.FinalizePoS()
+		}
 		return nil
 	}
 	newHeadBlock := api.eth.BlockChain().GetBlockByHash(newHead)
